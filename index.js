131fdb72f1b563b9d04664c20c0cce0bcf7cb274
necto68 <roman.horodianenko@gmail.com>
2018-01-21 14:54:54 +0200

    some fix, prettier

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-const express = require('express');
-const crypto = require('crypto');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let dataArray = [];
-let blocksObject= {};
+const dataArray = [];
+const blocksObject = {};
 
 const createBlock = () => {
-    let blocksSortedArray = Object.values(blocksObject).sort((a, b) => a.timestamp - b.timestamp);
-    const previousBlockHash = blocksSortedArray.length ? blocksSortedArray[blocksSortedArray.length - 1].block_hash : '0';
-
+    const blocksSortedArray = Object.values(blocksObject).sort(
+        (a, b) => a.timestamp - b.timestamp
+    );
+    const previousBlockHash = blocksSortedArray.length
+        ? blocksSortedArray[blocksSortedArray.length - 1].block_hash
+        : "0";
     const blockData = [...dataArray].splice(-3);
-    const currentBlockHash = crypto.createHash('sha256').update(blockData.join('')).digest('hex');
+    const currentBlockHash = crypto
+        .createHash("sha256")
+        .update(blockData.join(""))
+        .digest("hex");
 
     blocksObject[currentBlockHash] = {
         previous_block_hash: previousBlockHash,
         rows: [...dataArray].splice(-5),
         timestamp: Date.now(),
-        block_hash: currentBlockHash,
-    }
+        block_hash: currentBlockHash
+    };
 };
 
-app.post('/add_data', (req, res) => {
+app.post("/add_data", (req, res) => {
     const { data } = req.body;
+
     dataArray.push(data);
 
     if (dataArray.length % 5 === 0) {
         createBlock();
     }
 
-    res.send('data saved');
+    res.send("data saved");
 });
 
-app.get('/last_blocks/:lastBlocksLength', (req, res) => {
+app.get("/last_blocks/:lastBlocksLength", (req, res) => {
     const { lastBlocksLength } = req.params;
 
     res.json(
-        Object
-            .values(blocksObject)
+        Object.values(blocksObject)
             .sort((a, b) => b - a)
             .splice(-lastBlocksLength)
     );
